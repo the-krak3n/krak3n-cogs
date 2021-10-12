d32ab52dfacdb931587473fc6797fd8549287968
Jefferson Jr Shim <71493766+JeffJrShim@users.noreply.github.com>
2021-10-12 16:53:42 +0800

    make pp size stonks

--- penis/penis.py
+++ penis/penis.py
@@ -28,8 +28,8 @@ class Penis(commands.Cog):
             title="Penis size machine ",color=await ctx.embed_color()
         )     
         if user.id == self.bot.user.id or self.bot.owner_ids:
-            embed.description=(f"**{user.name}'s penis:**\n8{'=' * random.randint(25,45)}D")
+            embed.description=(f"**{user.name}'s penis:**\n8{'=' * random.randint(45,90)}D")
         else:
-            embed.description=(f"**{user.name}'s penis:**\n8{'=' * random.randint(0, 35)}D")
+            embed.description=(f"**{user.name}'s penis:**\n8{'=' * random.randint(0, 10)}D")
 
         await ctx.reply(embed=embed,mention_author=False)   
